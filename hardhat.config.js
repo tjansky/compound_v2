f2ebc94d0099996048502473de26788ed15f181b
TJANSKY\tomis <tomislav.jansky@gmail.com>
2025-04-15 22:59:29 +0200

    added hardhat config for pulsechain

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,11 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.20",
+  networks: {
+    pulsechain: {
+      url: "https://rpc.pulsechain.com",  // Mainnet RPC
+      chainId: 369,
+      accounts: [PRIVATE_KEY] // Load private key from .env
+    }
+  }
 };
